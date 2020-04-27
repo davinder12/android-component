cb1f57313e738f3ef6e8cbc009c1eebe4b7ae83f
davinder <davindersyal12@gmail.com>
2020-04-27 16:13:36 +0530

    Integrate click Listener on adapter (done)

--- uiview/src/main/java/com/sdi/joyersmajorplatform/uiview/recyclerview/DataBoundAdapterClass.kt
+++ uiview/src/main/java/com/sdi/joyersmajorplatform/uiview/recyclerview/DataBoundAdapterClass.kt
@@ -7,14 +7,22 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
-abstract class DataBoundAdapterClass<T, V : ViewDataBinding>(diffUtil: DiffUtil.ItemCallback<T>) :
-    ListAdapter<T, DataBoundViewHolder<*>>(diffUtil) {
+abstract class DataBoundAdapterClass<T, V : ViewDataBinding>(diffUtil: DiffUtil.ItemCallback<T>,
+                                                             private val enableClicks: Boolean = true) :
+    ListAdapter<T, DataBoundViewHolder<*>>(diffUtil) ,IAdapter<T>{
     
     companion object {
         const val DEFAULT_LAYOUT = 84740
     }
-    
+
+    private val clickSource = PublishSubject.create<T>()
+    override val clicks: Observable<T> = clickSource.throttleFirst(500, TimeUnit.MILLISECONDS)
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<*> {
         return when (viewType) {
             DEFAULT_LAYOUT -> DataBoundViewHolder(createBinding(parent, defaultLayoutRes))
@@ -31,14 +39,30 @@ abstract class DataBoundAdapterClass<T, V : ViewDataBinding>(diffUtil: DiffUtil.
      * Override this to customize the view binding
      */
     protected open fun createBinding(parent: ViewGroup, @LayoutRes layoutRes: Int): V {
-      return DataBindingUtil.inflate<V>(
+      val binding = DataBindingUtil.inflate<V>(
             LayoutInflater.from(parent.context),
             layoutRes,
             parent,
             false
         )
-        
+
+        if (enableClicks) {
+            binding.root.setOnClickListener {
+                onClick(binding)
+            }
+        }
+        return  binding
     }
+
+    var onClickListener: (T) -> Unit = clickSource::onNext
+
+
+    protected fun onClick(binding: V) {
+        map(binding)?.let { onClickListener(it) }
+    }
+
+
+
 
     override fun getItemViewType(position: Int): Int {
         return getCustomItemViewType(position)
@@ -60,6 +84,15 @@ abstract class DataBoundAdapterClass<T, V : ViewDataBinding>(diffUtil: DiffUtil.
 
     protected open fun getCustomItemViewType(position: Int): Int {
         return DEFAULT_LAYOUT
+    }
+
+    /**
+     * Should return the bound item from a binding.
+     * This is used to attach a click listener
+     */
+    open fun map(binding: V): T? {
+        // TODO: log
+        return null
     }
 
 }
---
+++ uiview/src/main/java/com/sdi/joyersmajorplatform/uiview/recyclerview/IAdapter.kt
@@ -0,0 +1,10 @@
+package com.sdi.joyersmajorplatform.uiview.recyclerview
+
+import io.reactivex.Observable
+
+
+interface IAdapter<T> {
+    val clicks: Observable<T>
+    val longClicks: Observable<T>
+    fun submit(list: List<T>?, callback: Runnable? = null)
+}
