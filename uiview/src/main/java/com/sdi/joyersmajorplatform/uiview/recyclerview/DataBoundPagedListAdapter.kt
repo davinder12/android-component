17fc15a696d8f407ccaa70a70059fcb748a5419b
davinder <davindersyal12@gmail.com>
2020-05-12 12:17:54 +0530

    Rename DataBound PageList Adapter (done)

--- uiview/src/main/java/com/sdi/joyersmajorplatform/uiview/recyclerview/DataBoundPagedListAdapter.kt
+++ uiview/src/main/java/com/sdi/joyersmajorplatform/uiview/recyclerview/DataBoundPagedListAdapter.kt
@@ -5,34 +5,84 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
 import androidx.paging.PagedListAdapter
-
 import androidx.recyclerview.widget.DiffUtil
+import com.sdi.joyersmajorplatform.uiview.NetworkState
+import com.sdi.joyersmajorplatform.uiview.R
+import com.sdi.joyersmajorplatform.uiview.databinding.NetworkStateItemBinding
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
-abstract class DataBoundPagedListAdapter<T, V : ViewDataBinding>(diffUtil: DiffUtil.ItemCallback<T>) :
-    PagedListAdapter<T, DataBoundViewHolder<V>>(diffUtil) {
-
-    abstract var itemlayout: Int
-
+abstract class DataBoundPagedListAdapter<T, V : ViewDataBinding>(diffUtil: DiffUtil.ItemCallback<T>,
+                                                                 private val endLoadingIndicator: Boolean = true,
+                                                                 var lifecycleOwner: LifecycleOwner? = null,
+                                                                 private val frontLoadingIndicator: Boolean = true,
+                                                                 private val enableClicks: Boolean = true) :
+    PagedListAdapter<T, DataBoundViewHolder<*>>(diffUtil) , IAdapter<T> {
 
     companion object {
         const val DEFAULT_LAYOUT = 84740
-     
+        const val FRONT_LOADING_INDICATOR = 84741
+        const val END_LOADING_INDICATOR = 84742
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<V> {
-        return when(viewType){
-             DEFAULT_LAYOUT -> DataBoundViewHolder(createBinding(parent,itemlayout))
-             else-> DataBoundViewHolder(createBinding(parent,getLayoutForViewType(viewType)))
-         }
+    private var endLoadingState: NetworkState? = null
+    private var frontLoadingState: NetworkState? = null
+
+    private val clickThrottle: Long = 500L
+
+
+    @LayoutRes
+    private val networkStateRes = R.layout.network_state_item
+
+
+    private val clickSource = PublishSubject.create<T>()
+    override val clicks: Observable<T> = clickSource.throttleFirst(500, TimeUnit.MILLISECONDS)
+
+    protected val retryClickSource = PublishSubject.create<NetworkState>()
+    val retryClicks: Observable<NetworkState> = retryClickSource.throttleFirst(clickThrottle, TimeUnit.MILLISECONDS)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<*> {
+        return when (viewType) {
+            DEFAULT_LAYOUT -> DataBoundViewHolder(createBinding(parent, defaultLayoutRes))
+            FRONT_LOADING_INDICATOR, END_LOADING_INDICATOR -> DataBoundViewHolder(
+                createNetworkBinding(parent)
+            )
+            else -> DataBoundViewHolder(createBinding(parent, getLayoutForViewType(viewType)))
+        }
     }
 
-    override fun onBindViewHolder(holder: DataBoundViewHolder<V>, position: Int) {
-        bind(holder.binding, getItem(position), position)
-        holder.binding.executePendingBindings()
+    override fun onBindViewHolder(holder: DataBoundViewHolder<*>, position: Int) {
+        holder.binding.lifecycleOwner = lifecycleOwner
+        when (getItemViewType(position)) {
+            FRONT_LOADING_INDICATOR -> {
+                (holder.binding as NetworkStateItemBinding).item = frontLoadingState
+            }
+            END_LOADING_INDICATOR -> {
+                (holder.binding as NetworkStateItemBinding).item = endLoadingState
+            }
+            else -> {
+                val actualPosition = if (isLoadingAtFront()) position - 1 else position
+                bind(holder.binding as V, getItem(actualPosition), position)
+            }
+        }
     }
 
 
+    private fun createNetworkBinding(parent: ViewGroup): NetworkStateItemBinding {
+        val binding = NetworkStateItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        binding.retryButton.setOnClickListener {
+            binding.item?.let { retryClickSource.onNext(it) }
+        }
+        return binding
+    }
 
     /**
      * Override this to customize the view binding
@@ -42,18 +92,89 @@ abstract class DataBoundPagedListAdapter<T, V : ViewDataBinding>(diffUtil: DiffU
             LayoutInflater.from(parent.context),
             layoutRes,
             parent,
-            false)
+            false
+        )
 
-        return binding
+        if (enableClicks) {
+            binding.root.setOnClickListener {
+                onClick(binding)
+            }
+        }
+        return  binding
+    }
+
+    var onClickListener: (T) -> Unit = clickSource::onNext
+
+
+    protected fun onClick(binding: V) {
+        map(binding)?.let { onClickListener(it) }
     }
 
 
-    override fun getItemViewType(position: Int): Int {
-        return getCustomItemViewType(position)
+    private fun isLoadingAtEnd() =
+        endLoadingState != null && endLoadingState != NetworkState.success
+
+    private fun isLoadingAtFront() =
+        frontLoadingState != null && frontLoadingState != NetworkState.success
+
+
+    private fun getExtraRows(): Int {
+        var count = 0
+        if (isLoadingAtEnd()) ++count
+        if (isLoadingAtFront()) ++count
+        return count
+    }
+
+    override fun getItemCount(): Int {
+        return super.getItemCount() + getExtraRows()
+    }
+
+    open fun updateEndLoadingState(newPagingState: NetworkState?) {
+        if (!endLoadingIndicator || isLoadingAtFront()) return
+        val previousState = this.endLoadingState
+        val hadExtraRow = isLoadingAtEnd()
+        this.endLoadingState = newPagingState
+        val hasExtraRow = isLoadingAtEnd()
+        if (hadExtraRow != hasExtraRow) {
+            if (hadExtraRow) {
+                notifyItemRemoved(itemCount + 1)
+            } else {
+                notifyItemInserted(itemCount)
+            }
+        } else if (hasExtraRow && previousState != newPagingState) {
+            notifyItemChanged(itemCount)
+        }
+    }
+
+    open fun updateFrontLoadingState(newPagingState: NetworkState?) {
+        if (!frontLoadingIndicator || isLoadingAtEnd()) return
+        val previousState = this.frontLoadingState
+        val hadExtraRow = isLoadingAtFront()
+        this.frontLoadingState = newPagingState
+        val hasExtraRow = isLoadingAtFront()
+        if (hadExtraRow != hasExtraRow) {
+            if (hadExtraRow) {
+                notifyItemRemoved(0)
+            } else {
+                notifyItemInserted(0)
+            }
+        } else if (hasExtraRow && previousState != newPagingState) {
+            notifyItemChanged(0)
+        }
     }
 
 
-    abstract fun bind(bind: V, itemType: T?, position: Int)  
+    final override fun getItemViewType(position: Int): Int {
+        return if (isLoadingAtEnd() && position == itemCount - getExtraRows()) {
+            END_LOADING_INDICATOR
+        } else if (isLoadingAtFront() && position == 0) {
+            FRONT_LOADING_INDICATOR
+        } else {
+            getCustomItemViewType(position)
+        }
+    }
+
+    abstract fun bind(bind: V, itemType: T?, position: Int)
 
     /**
      * The [LayoutRes] for the RecyclerView item
@@ -62,7 +183,6 @@ abstract class DataBoundPagedListAdapter<T, V : ViewDataBinding>(diffUtil: DiffU
     protected abstract val defaultLayoutRes: Int
         @LayoutRes get
 
-
     @LayoutRes
     protected open fun getLayoutForViewType(viewType: Int): Int {
         return defaultLayoutRes
@@ -70,6 +190,17 @@ abstract class DataBoundPagedListAdapter<T, V : ViewDataBinding>(diffUtil: DiffU
 
     protected open fun getCustomItemViewType(position: Int): Int {
         return DEFAULT_LAYOUT
+    }
+
+
+    /**
+     * Should return the bound item from a binding.
+     * This is used to attach a click listener
+     */
+    open fun map(binding: V): T? {
+        // TODO: log
+        return null
+
     }
 
 }
